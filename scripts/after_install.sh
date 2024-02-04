1b180959b0647de793d1e902f98c051af56bbd2f
Parth Sinha <41264578+sinhaparth5@users.noreply.github.com>
2024-02-04 16:57:05 +0000

    deleted the script

--- scripts/after_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/threejs-portfolio-website/deploy.log
-
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/threejs-portfolio-website/deploy.log
-cd /home/ec2-user/threejs-portfolio-website >> /home/ec2-user/threejs-portfolio-website/deploy.log
-
-echo 'npm install' >> /home/ec2-user/threejs-portfolio-website/deploy.log 
-npm install >> /home/ec2-user/threejs-portfolio-website/deploy.log
--- scripts/application_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo 'run application_start.sh: ' >> /home/ec2-user/threejs-portfolio-website/deploy.log
-
-echo 'pm2 restart my-vite-dev-server' >> /home/ec2-user/threejs-portfolio-website/deploy.log
-pm2 restart my-vite-dev-server >> /home/ec2-user/threejs-portfolio-website/deploy.log
