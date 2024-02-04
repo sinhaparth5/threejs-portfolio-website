35cc40b78984f08d0f75ece7f4aa377c89b9d7d9
Parth Sinha <41264578+sinhaparth5@users.noreply.github.com>
2024-02-04 12:37:03 +0000

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,4 +3,4 @@
 echo 'run application_start.sh: ' >> /home/ec2-user/threejs-portfolio-website/deploy.log
 
 echo 'pm2 restart my-vite-dev-server' >> /home/ec2-user/threejs-portfolio-website/deploy.log
-pm2 restart nodejs-express-app >> /home/ec2-user/threejs-portfolio-website/deploy.log
+pm2 restart my-vite-dev-server >> /home/ec2-user/threejs-portfolio-website/deploy.log
